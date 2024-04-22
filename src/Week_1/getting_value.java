bdd2624eeaa9c51bfca189ae4efdc5c28c852049
oguzhansali61 <salioguzhan.61@gmail.com>
2024-04-22 09:11:16 +0300

    üçüncü bir değer girilerek işlem tekrarlandı.

--- src/Week_1/getting_value.java
+++ src/Week_1/getting_value.java
@@ -1,2 +1,27 @@
-package Week_1;public class getting_value {
+package Week_1;
+import java.util.Scanner;
+
+public class getting_value {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Birinci sayıyı giriniz:");
+        int sayi1 = scanner.nextInt();
+
+        System.out.println("İkinci sayıyı giriniz:");
+        int sayi2 = scanner.nextInt();
+
+
+        System.out.println("Ucuncu sayiyi giriniz:");
+        int sayi3 = scanner.nextInt();
+
+        // İkinci sayı sıfır mı diye kontrol ediyoruz
+        if (sayi2 != 0) {
+            double sonuc = (double) (sayi1 / sayi2)+ sayi3; // Double türüne dönüştürüyoruz
+            System.out.println(" işleminin sonucu: " + sonuc);
+        } else {
+            System.out.println("Bir sayı sıfıra bölünemez!");
+        }
+    }
+
 }
