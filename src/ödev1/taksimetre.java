e7eaf33087ab7e1dd6022db450b835be03e5e54a
oguzhansali61 <salioguzhan.61@gmail.com>
2024-04-22 17:02:41 +0300

    Girilen veriye göre Taksimetre ücreti hesaplama.

---
+++ src/ödev1/taksimetre.java
@@ -0,0 +1,26 @@
+package ödev1;
+
+import java.util.Scanner;// kullanıcıdan veri alınması için scanner import edildi.
+
+public class taksimetre {
+    public static void main(String[] args) {// kodun çalıştırılamsı çin main bloğu eklendi.
+
+        int km;// Değişken tanımlandı.
+        Scanner inp = new Scanner(System.in);// veri almaya yarayan scanner tanımlandı.
+
+        System.out.println("Gidilen yol: ");// Veri alındı.
+        km = inp.nextInt();//  Alınan değişken tanımlandı.
+
+        double acılıs = 10;// Değişken tanımlandı.
+        double tutar = (km*2.20)+ acılıs;// Değişken tanımlandı.
+
+        if(tutar < 20){//Tutara göre fiyat belirlemesi yapıldı.
+            System.out.println("Ödenecek Tutar: " + (20) + " TL dir.");
+
+
+        }else if (tutar>20){
+            System.out.println("Ödenecek Tutar: " + (tutar)+ " TL dir." );
+        }
+
+    }
+}
