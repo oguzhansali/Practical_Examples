9f541b86023aaa2c78187db366b8f69c70c39355
oguzhansali61 <salioguzhan.61@gmail.com>
2024-04-22 18:41:13 +0300

    Boy ve kilo değerleri alınarak kütle indeksi hesaplaması yapıldı.

---
+++ src/ödev1/kitle_endeks.java
@@ -0,0 +1,27 @@
+package ödev1;
+
+import java.util.Scanner;// Scanner kullanimi için import edildi.
+
+public class kitle_endeks {
+    public static void main(String[] args) {// main blogu eklendi.
+        // Değişkenler tanımlandi.
+        double boy;
+        double kilo;
+
+        Scanner input = new Scanner(System.in);//Veri alimi için scanner tanımlandi.
+
+        //Kullanicidan boy degeri alindi.
+        System.out.println("Lütfen Boyunuzu (metre cinsinde) giriniz: ");
+        boy = input.nextDouble();
+        //Kullanicidan kilo degeri alindi.
+        System.out.println("Lütfen Kilonuzu (Kilogram cinsinden) giriniz: ");
+        kilo = input.nextDouble();
+        //Indeks hesaplandi.
+        double indeks =(kilo/(boy*boy));
+        //Indeks ekrana yazdirildi.
+        System.out.println("Vücut Kitle Indeksiniz: " + indeks);
+
+
+
+    }
+}
