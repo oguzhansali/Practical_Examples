4fcd6531a495dbe20f5278a9ecb270ff51caef79
oguzhansali61 <salioguzhan.61@gmail.com>
2024-04-26 21:54:26 +0300

    .

--- src/Week_1/odev3/TekSayilarinToplami.java
+++ src/Week_1/odev3/TekSayilarinToplami.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class TekSayilarinToplami {
     public static void main(String[] args) {
+        //Degıskenler tanımladı.
         int i;
         int sum =0;
         Scanner input = new Scanner(System.in);
-
+// Çift ve 4un katı olan çiftleri bulan kosul olusturuldu.
         do{
             System.out.print("Bir sayi giriniz :");
             i = input.nextInt();
@@ -16,8 +17,8 @@ public class TekSayilarinToplami {
 
             }
 
-        }while(i%2==0);
-        System.out.println(sum);
+        }while(i%2==0);//Bu koşul bozulana kadar çalışmaya devam edecek.
+        System.out.println("Çift ve 4'ün katı sayıların toplamı : " + sum);//Çıktı yazdırıldı.
 
 
 
