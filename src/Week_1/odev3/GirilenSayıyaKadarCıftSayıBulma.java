0d8b22b59dd8ce7b7fee158a945260a989e28f75
oguzhansali61 <salioguzhan.61@gmail.com>
2024-04-26 21:46:35 +0300

    .

--- src/Week_1/odev3/GirilenSayıyaKadarCıftSayıBulma.java
+++ src/Week_1/odev3/GirilenSayıyaKadarCıftSayıBulma.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class GirilenSayıyaKadarCıftSayıBulma {
     public static void main(String[] args) {
+        //Degıskenler tanımlandı ve kullanıcıdan veri alındı.
         int k ;
         Scanner input = new Scanner(System.in);
         System.out.println("Bir sayi giriniz : ");
         k = input.nextInt();
+        //Sonuca ulaşıalcak döngü oluşturuldu.
         int i = 1;
         while (i<=k){
             i++;
             if (i % 2 == 0){
-                System.out.println(i);
+                System.out.println(i);//Sonuc yazdırıldı.
             }
         }
 
