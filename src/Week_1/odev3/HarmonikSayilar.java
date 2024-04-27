8bbe1d016fa74b40952f5ba7ba74cd8992c0e479
oguzhansali61 <salioguzhan.61@gmail.com>
2024-04-27 14:57:45 +0300

    .

---
+++ src/Week_1/odev3/HarmonikSayilar.java
@@ -0,0 +1,29 @@
+package Week_1.odev3;
+
+import java.util.Scanner;
+
+public class HarmonikSayilar {
+    public static void main(String[] args) {
+        //Degıskneler tanımladı.
+        int sayi;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sayı Giriniz: ");
+        sayi = input.nextInt();
+        double toplam = 0;//Degiskende deger tanımlandı
+//Pozitif sayı için harmonik hesaplama yapıldı.
+        if (sayi > 0) {
+            for (int i = 1; i <= sayi; i++) {
+                toplam += 1.0 / i;
+            }
+            System.out.println(sayi + " Harmonik Sayısının  Toplamı :  " + toplam);
+//Negatif sayı için harmonik hesaplama yapıldı.
+        } else if (sayi < 0) {
+            for (int i = -1; i >= sayi; i--) {
+                toplam += 1.0 / i;
+            }
+            System.out.println(sayi + " Harmonik Sayısının Farkı :  " + toplam);
+        } else {//0 olduğu kosul için sonuc yazıldı.
+            System.out.println("Sonuc 1 dir.");
+        }
+    }
+}
