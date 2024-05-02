5d8853487e8ea95b5534060a97cf7a3a29b7fdcf
oguzhansali61 <154914330+oguzhansali61@users.noreply.github.com>
2024-05-02 17:09:52 +0300

    Yorum satıları eklendi.

--- src/Week_2/Homework/DizideOrtalamaBulma.java
+++ src/Week_2/Homework/DizideOrtalamaBulma.java
@@ -2,9 +2,9 @@ package Week_2.Homework;
 
 public class DizideOrtalamaBulma {
     public static void main(String[] args) {
-        int[] list = {2,3,4,5,6};
-        double sum=0;
-
+        int[] list = {2,3,4,5,6};//Dizi oluşturuldu.
+        double sum=0;//Toplam başlangıç 0 tanımlandı.
+//Liste içindeki  indeksler teker teker gezilerek toplama işlemi sum değerine atandı ve sum değerinin harmonik ortalaması alındı.
         for (int i = 0; i< list.length;i++){
             sum+=(1.0/list[i]);
         }
